50588c7694ce2b6130a43988dc0573eaec4459c0
David Walker <david_walker1@hotmail.co.uk>
2023-06-22 18:08:21 +0100

    add appbar to root file

--- src/routes/root.tsx
+++ src/routes/root.tsx
@@ -1,11 +1,31 @@
 
+import { AppBar, Box, Button, Toolbar, Typography } from "@mui/material";
+import React from "react";
 import { Outlet } from "react-router-dom";
+import theme from "../lib/theme";
 
-
-export const Root = () => {
+export const Root: React.FC = () => {
   return (
-    <>
-
-    </>
+    <Box sx={{ flexGrow: 1 }}>
+      <AppBar
+        position="static"
+        sx={{ backgroundColor: theme.palette.primary.light }}
+      >
+        <Toolbar>
+          <Typography
+            variant="h6"
+            component="div"
+            sx={{ flexGrow: 1 }}
+            color={theme.palette.primary.contrastText}
+          >
+            Art Gallery
+          </Typography>
+          <Button sx={{ color: theme.palette.primary.contrastText }}>
+            Login
+          </Button>
+        </Toolbar>
+      </AppBar>
+      <Outlet />
+    </Box>
   );
 };
