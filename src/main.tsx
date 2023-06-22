a6f9354dfca752619d0a8034a6be8c3b9c616670
David Walker <david_walker1@hotmail.co.uk>
2023-06-22 18:07:29 +0100

    set up basic router and wrap app in theme

--- src/main.tsx
+++ src/main.tsx
@@ -2,18 +2,20 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import './index.css'
 import { RouterProvider, createBrowserRouter } from 'react-router-dom'
+import { Root } from "./routes/Root";
+import { ThemeProvider } from "@emotion/react";
+import theme from "./lib/theme";
 
 const router = createBrowserRouter([
   {
     path: "/",
     // element: <Root />,
-    element: <div>Home Page</div>,
+    element: <Root />,
     children: [
       // {
       //   index: true,
       //   element: <HomeScreen />,
       // },
-     
     ],
   },
   // {
@@ -22,8 +24,10 @@ const router = createBrowserRouter([
   // },
 ]);
 
-ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
+ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <React.StrictMode>
-   <RouterProvider router={router}/>
-  </React.StrictMode>,
-)
+    <ThemeProvider theme={theme}>
+      <RouterProvider router={router} />
+    </ThemeProvider>
+  </React.StrictMode>
+);
