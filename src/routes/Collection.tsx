9ec9131cba6a207b37da4c79478515e6868ab917
David Walker <david_walker1@hotmail.co.uk>
2023-06-26 15:03:12 +0100

    create collection page reusing gallery component

---
+++ src/routes/Collection.tsx
@@ -0,0 +1,31 @@
+import { Container, Typography } from "@mui/material";
+import React from "react";
+import { useAppContext } from "../contexts/appContext";
+import { Gallery } from "../components/Gallery";
+import { Image } from "../types/image";
+
+export const Collection: React.FC = () => {
+  const { userCollection, removeArtwork } = useAppContext();
+  const handleRemoveArtwork = (artwork: Image) => {
+    removeArtwork(artwork.id);
+  };
+  return (
+    <Container>
+      <Typography
+        variant="h1"
+        textAlign={"center"}
+        fontSize={26}
+        fontWeight={600}
+        color={"primary"}
+        mt={5}
+      >
+        My Collection
+      </Typography>
+      <Gallery
+        images={userCollection ? userCollection : []}
+        isCollection
+        handleRemoveArtwork={handleRemoveArtwork}
+      />
+    </Container>
+  );
+};
